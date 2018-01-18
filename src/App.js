caf36c9a7c6dd57e552d608ab1180d04179a44c2
Quentin BENYAHIA <quentin.benyahia@gmail.com>
2018-01-18 09:31:22 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import {SideBar} from '@sketchpixy/rubix';
+import './main.css';
+
+class App extends Component {
+  render() {
+    return (
+        <SideBar>
+
+        </SideBar>
+    );
+  }
+
+
+
+
+
+}
+
+export default App;
